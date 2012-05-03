07904b83a35352e2e080419bc0e046d279e1b670
William Woodall <wjwwood@gmail.com>
2012-05-02 23:39:21 -0500

    Move documentation updates

--- docs/v1.0/search/all_61.js
+++ docs/v1.0/search/all_61.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['available',['available',['../classserial_1_1_serial.html#afafe25b2f3bb0809550abdc72c51a234',1,'serial::Serial::available()'],['../classserial_1_1serial_1_1_serial_1_1_serial_impl.html#aec474ec54cb40a4191939aacb80c5ea2',1,'serial::serial::Serial::SerialImpl::available()'],['../classserial_1_1serial_1_1_serial_1_1_serial_impl.html#aecd5e068c21b076bcf161f7bf7f415f5',1,'serial::serial::Serial::SerialImpl::available()']]]
+  ['available',['available',['../classserial_1_1serial_1_1_serial_1_1_serial_impl.html#aec474ec54cb40a4191939aacb80c5ea2',1,'serial::serial::Serial::SerialImpl::available()'],['../classserial_1_1serial_1_1_serial_1_1_serial_impl.html#aecd5e068c21b076bcf161f7bf7f415f5',1,'serial::serial::Serial::SerialImpl::available()'],['../classserial_1_1_serial.html#afafe25b2f3bb0809550abdc72c51a234',1,'serial::Serial::available()']]]
 ];
