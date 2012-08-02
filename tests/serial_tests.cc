485a9fb9a3cd3ff90b01f5e9e3bdfef0356877bd
William Woodall <wjwwood@gmail.com>
2012-08-01 20:09:20 -0500

    Updated the tests for the new Serial API.

--- tests/serial_tests.cc
+++ tests/serial_tests.cc
@@ -53,7 +53,7 @@ protected:
     ASSERT_TRUE(slave_fd > 0);
     ASSERT_TRUE(string(name).length() > 0);
 
-    port1 = new Serial(string(name), 115200, 250);
+    port1 = new Serial(string(name), 115200, Timeout::simpleTimeout(250));
   }
 
   virtual void TearDown() {
