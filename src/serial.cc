1c1c43e97fa98051ccc6494cf0facf303f119e5b
Patrick McMichael <mcmichaelps1@gmail.com>
2015-06-15 12:49:01 -0400

    Update to Serial::setTimeout (and unix/win implementations) to accept timeout by const reference (Addresses issue #101)

--- src/serial.cc
+++ src/serial.cc
@@ -63,7 +63,7 @@ private:
   SerialImpl *pimpl_;
 };
 
-Serial::Serial (const string &port, uint32_t baudrate, serial::Timeout timeout,
+Serial::Serial (const string &port, uint32_t baudrate, const serial::Timeout &timeout,
                 bytesize_t bytesize, parity_t parity, stopbits_t stopbits,
                 flowcontrol_t flowcontrol)
  : pimpl_(new SerialImpl (port, baudrate, bytesize, parity,
@@ -279,7 +279,7 @@ Serial::getPort () const
 }
 
 void
-Serial::setTimeout (serial::Timeout &timeout)
+Serial::setTimeout (const serial::Timeout &timeout)
 {
   pimpl_->setTimeout (timeout);
 }
