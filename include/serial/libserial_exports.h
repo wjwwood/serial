e2b713e8d681cbff2faac8d3403edfc958c8848a
CristiFati <fati_utcluj@yahoo.com>
2022-04-14 22:46:46 +0300

    Win .dll build support

---
+++ include/serial/libserial_exports.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#if defined(_WIN32)
+#  if defined(LIBSERIAL_STATIC)
+#    define LIBSERIAL_EXPORT_API
+#  else
+#    if defined(LIBSERIAL_EXPORTS)
+#      define LIBSERIAL_EXPORT_API __declspec(dllexport)
+#    else
+#      define LIBSERIAL_EXPORT_API __declspec(dllimport)
+#    endif
+#  endif
+#else
+#  define LIBSERIAL_EXPORT_API
+#endif
+
